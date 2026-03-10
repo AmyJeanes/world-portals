649362f51677865d2380a11fa2427a403c4e573c
FuzzyLeo <leobarlow24@gmail.com>
2026-03-10 00:08:11 +0000

    scale and colour support

--- lua/worldportals/falseworld_sh.lua
+++ lua/worldportals/falseworld_sh.lua
@@ -12,8 +12,15 @@ wp.falseworlds.example = {
     models = {
         vending = {
             model = "models/props_interiors/VendingMachineSoda01a.mdl",
-            pos = Vector(-180,0,-3),
-            angle = Angle(0,0,0)
+            pos = Vector(-180,70,-3),
+            angle = Angle(0,0,0),
+            color = Vector(0,1,0)
+        },
+        tube = {
+            model = "models/props_phx/construct/windows/window_curve360x2.mdl",
+            pos = Vector(-180,-70,-50),
+            angle = Angle(0,0,0),
+            scale = Vector(0.5,0.5,1.7)
         },
         tower = {
             model = "models/props_phx/huge/tower.mdl",
@@ -62,9 +69,11 @@ function wp.createfalseworld(portal, plyOrigin, plyAngle, width, height, fov)
         local function DrawPart(rawpart)
             local skybox = rawpart.skybox
             local model = rawpart.model
+            local scale = rawpart.scale
+            local color = rawpart.color or Vector(1,1,1)
             local pos = rawpart.pos or Vector()
             local angle = rawpart.angle or Angle()
-            local rendergroup = rawpart.rendergroup or "RENDERGROUP_OPAQUE"
+            local rendergroup = rawpart.rendergroup
             local part = ClientsideModel(model, rendergroup)
             part:SetNoDraw(true)
             if skybox then
@@ -75,9 +84,15 @@ function wp.createfalseworld(portal, plyOrigin, plyAngle, width, height, fov)
             part:SetPoseParameter(1,pose or 0)
             local portalangle = portal:GetAngles()
             part:SetAngles(angle)
+            render.SetColorModulation(color.x,color.y,color.z)
             if not skybox then
                 render.ResetModelLighting(baselight.x,baselight.y,baselight.z)
                 render.SetLocalModelLights(falseworld.lights)
+            end
+            if scale then
+                local mat = Matrix()
+                mat:Scale(scale)
+                part:EnableMatrix("RenderMultiply", mat)
             end
             if skybox then
                 render.OverrideDepthEnable(true, false)
@@ -86,7 +101,7 @@ function wp.createfalseworld(portal, plyOrigin, plyAngle, width, height, fov)
             else
                 part:DrawModel()
             end
-            part:Remove()
+            part:Remove() -- clientside entity MUST be removed after drawing is completed otherwise they still stack up every single frame
         end
 
         if falseworld.skybox then
